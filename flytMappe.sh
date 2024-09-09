632e3110f0183b802c2366ffe24a7263eff9f00f
iklog <150009216+iklog@users.noreply.github.com>
2024-09-09 17:01:55 +0200

    Update flytMappe.sh

--- flytMappe.sh
+++ flytMappe.sh
@@ -1,32 +1,35 @@
-echo "Id of folder to copy FROM: "
-read fromFolder
-echo "Id of folder to copy TO: "
-read toFolder
-
-echo Trying to move subfolders from $fromFolder to $toFolder
-
 depth=0
 getFolders () {
-	./bin/gamadv-xtd3/gam user bend0006@brovstskole.dk print filelist select $1 showownedby any fields name,id showmimetype gfolder depth 0 > $1.csv
-	if [[ $(wc -l <$1.csv) -ge 2 ]]
-	then
-        	echo Der er subfolders
-		./bin/gamadv-xtd3/gam user bend0006@brovstskole.dk print filelist select $1 showownedby any fields name,id showmimetype not gfolder depth 0 |./bin/gamadv-xtd3/gam csv - gam user bvr@brovstskole.dk update drivefile id ~id parentid $2
+        ./bin/gamadv-xtd3/gam user $myUser print filelist select $1 showownedby any fields name,id showmimetype gfolder depth 0 > $1.csv
+        if [[ $(wc -l <$1.csv) -ge 2 ]]
+        then
+                echo Der er subfolders
+                echo From folder $1
+                echo To folder $2
+                ./bin/gamadv-xtd3/gam user $myUser print filelist select $1 showownedby any fields name,id showmimetype not gfolder depth 0 |./bin/gamadv-xtd3/gam csv - gam user $myUser update drivefile id ~id >
 
-		while IFS="," read -r owner id name 
-		do
-			echo $id
-			echo $name
-			./bin/gamadv-xtd3/gam user bvr@brovstskole.dk create drivefile drivefilename "$name" mimetype gfolder parentid $2 noduplicate > $id.out
-			#echo `cat $id.out|cut -d "(" -f2 | cut -d ")" -f1` 
-			#echo Newsubfolder: $newSubfolder 
-			getFolders $id `cat $id.out|cut -d "(" -f2 | cut -d ")" -f1`
-		done < <(tail -n +2 $1.csv)
-	else
-        	echo Der er ingen subfolders
-		./bin/gamadv-xtd3/gam user bend0006@brovstskole.dk print filelist select $1 showownedby any fields name,id showmimetype not gfolder depth 0 | ./bin/gamadv-xtd3/gam csv - gam user bvr@brovstskole.dk update drivefile id ~id parentid $2
+                while IFS="," read -r owner id name 
+                do
+                        echo $id
+                        echo $name
+                        ./bin/gamadv-xtd3/gam user bvr@brovstskole.dk create drivefile drivefilename "$name" mimetype gfolder parentid $2 noduplicate > $id.out 2> $id.fail 
+                        #echo `cat $id.out|cut -d "(" -f2 | cut -d ")" -f1` 
+                        #echo Newsubfolder: $newSubfolder 
+                        if [ -s $id.out ]; then
+                                #The subfolder was created.
+                                getFolders $id `cat $id.out|cut -d "(" -f2 | cut -d ")" -f1`
+                        else
+                                #The subfolder allready exists
+                                getFolders $id `cat $id.fail|rev|cut -d " " -f1 -|rev`
+                        fi
+                done < <(tail -n +2 $1.csv)
+        else
+                echo Der er ingen subfolders
+                echo From folder $1
+                echo To folder $2
+                ./bin/gamadv-xtd3/gam user $myUser print filelist select $1 showownedby any fields name,id showmimetype not gfolder depth 0 |./bin/gamadv-xtd3/gam csv - gam user $myUser update drivefile id ~id >
 
-	fi
+        fi
 }
 
 getFolders $fromFolder $toFolder
